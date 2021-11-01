8ab80ccdebadf4a4544c5c001cfa201f9de98aba
Wellington Caires <well.caires@hotmail.com>
2021-11-01 11:55:14 -0300

    Update title

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -23,7 +23,7 @@ function App() {
         </div>
 
         <div className="py-3">
-          <h2 className="text-primary">Todas vendas</h2>
+          <h2 className="text-primary">Todas Vendas</h2>
         </div>
 
         <DataTable />
