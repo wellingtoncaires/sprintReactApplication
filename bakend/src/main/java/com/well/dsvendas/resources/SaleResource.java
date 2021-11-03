8f203b383b321dc453ce238b348f078acc0a35ab
Wellington Caires <well.caires@hotmail.com>
2021-11-03 15:49:02 -0300

    Pagination

---
+++ bakend/src/main/java/com/well/dsvendas/resources/SaleResource.java
@@ -0,0 +1,27 @@
+package com.well.dsvendas.resources;
+
+import com.well.dsvendas.dto.SaleDTO;
+import com.well.dsvendas.services.SaleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/sales")
+public class SaleResource {
+
+    @Autowired
+    private SaleService service;
+
+    @GetMapping
+    public ResponseEntity<Page<SaleDTO>> findAll(Pageable pageable) {
+        Page<SaleDTO> list = service.findAll(pageable);
+        return ResponseEntity.ok().body(list);
+    }
+}
